69a0f40dcc1241c56142f02495f538435a54c40b
tiwariDhawal <98008402+tiwariDhawal@users.noreply.github.com>
2022-06-19 12:24:10 +0530

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Route, Switch } from "react-router";
+
 function App() {
-  return <div>hi</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        This is the home page
+      </Route>
+      <Route exact path="/starred">
+        This is the starred page
+      </Route>
+      <Route>This is a 404 page</Route>
+    </Switch>
+  );
 }
 
 export default App;
