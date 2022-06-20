ff545a7bbc29fe857afacf117d5c80fdbc4e38dc
tiwariDhawal <98008402+tiwariDhawal@users.noreply.github.com>
2022-06-20 11:17:53 +0530

    displayed shows result and small refactor of fetch

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,40 @@
 import React, { useState } from "react";
 import MainPageLayout from "../components/MainPageLayout";
+import { apiGet } from "../misc/config";
 
 const Home = () => {
   const [input, setInput] = useState("");
+  const [results, setResults] = useState(null);
   const onInputChange = (event) => {
     setInput(event.target.value);
     // console.log(event.target.value);
   };
   const onSearch = () => {
     // https://api.tvmaze.com/search/shows?q=men
-
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-      .then((response) => response.json())
-      .then((result) => {
-        console.log(result);
-      });
+    apiGet(`/search/shows?q=${input}`).then((result) => {
+      setResults(result);
+    });
   };
   const onKeyDown = (event) => {
     if (event.keyCode === 13) {
       onSearch();
     }
     // console.log(event.keyCode);
+  };
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No results</div>;
+    }
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map((item) => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+    return null;
   };
   return (
     <MainPageLayout>
@@ -33,6 +47,7 @@ const Home = () => {
       <button type="button" onClick={onSearch}>
         Search
       </button>
+      {renderResults()}
     </MainPageLayout>
   );
 };
